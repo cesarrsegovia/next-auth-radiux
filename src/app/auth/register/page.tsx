a5ef2c43c9f9f288d001fa6e1d5f0937c321b69a
cesarrsegovia <cesarr.segovia27@gmail.com>
2025-07-21 23:30:42 -0300

    agregando registerform

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,8 +1,28 @@
+import { Container, Card, Heading, Flex, Text, Link } from "@radix-ui/themes";
+//import { BookmarkIcon } from "@radix-ui/react-icons";
+import  NavLink  from "next/link";
+import SignupForm from "@/components/auth/SignupForm";
+
 function RegisterPage() {
   return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
+    <>
+      <Container size="1" height="100%" className="p-3 md:p-0">
+        <Flex className="h-screen w-full items-center">
+            <Card className="w-full p-7">
+                <Heading>Sign Up</Heading>
+                <SignupForm/>
+                <Flex justify="between" my="4">
+                  <Text>
+                    Ya tienes cuenta?
+                  </Text>
+                  <Link asChild>
+                    <NavLink href="/auth/login" passHref>Logueate</NavLink>
+                  </Link>
+                </Flex>
+            </Card>
+        </Flex>
+      </Container>
+    </>
   );
 }
 
--- src/components/auth/SignupForm.tsx
+++ src/components/auth/SignupForm.tsx
@@ -1,16 +1,24 @@
 "use client"
 import { Flex, TextField, Button } from "@radix-ui/themes";
-import { EnvelopeClosedIcon, LockClosedIcon } from "@radix-ui/react-icons";
+import { EnvelopeClosedIcon, LockClosedIcon, PersonIcon } from "@radix-ui/react-icons";
 
-function SigninForm() {
+function SignupForm() {
   return (
     <Flex direction="column" gap="2">
+        <label htmlFor="name">Name:</label>
+        <TextField.Root>
+            <TextField.Slot>
+                <PersonIcon height="16" width="16"/>
+            </TextField.Slot>
+            <TextField.Root type="text" placeholder="Escribe tu nombre"/>
+        </TextField.Root>
+
         <label htmlFor="email">Email</label>
         <TextField.Root>
             <TextField.Slot>
                 <EnvelopeClosedIcon height="16" width="16"/>
             </TextField.Slot>
-            <TextField.Root type="email" placeholder="email@domain.com" autoFocus/>
+            <TextField.Root type="email" placeholder="hola@asd.com"/>
         </TextField.Root>
 
         <label htmlFor="password">Password</label>
@@ -21,10 +29,10 @@ function SigninForm() {
             <TextField.Root type="password" placeholder="*******"/>
         </TextField.Root>
         <Button>
-            Sign In
+            Sign Up
         </Button>
     </Flex>
   );
 }
 //seguir arreglando el formulario
-export default SigninForm;
+export default SignupForm;
