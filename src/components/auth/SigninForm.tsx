648dbcf6b6761c37427524adc3c67cd9306c6719
cesarrsegovia <cesarr.segovia27@gmail.com>
2025-07-22 23:18:30 -0300

    configurando validaciones

--- src/components/auth/SigninForm.tsx
+++ src/components/auth/SigninForm.tsx
@@ -1,30 +1,59 @@
 "use client"
-import { Flex, TextField, Button } from "@radix-ui/themes";
+import { Flex, TextField, Button, Text } from "@radix-ui/themes";
 import { EnvelopeClosedIcon, LockClosedIcon } from "@radix-ui/react-icons";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 
 function SigninForm() {
+
+    const { control, handleSubmit, formState: {errors} } = useForm();
+
+    const onSubmit = handleSubmit(data => {
+        console.log(data);
+    })
   return (
-    <Flex direction="column" gap="2">
+    <form onSubmit={onSubmit}>
+        <Flex direction="column" gap="2">
         <label htmlFor="email">Email</label>
         <TextField.Root>
             <TextField.Slot>
                 <EnvelopeClosedIcon height="16" width="16"/>
             </TextField.Slot>
-            <TextField.Root type="email" placeholder="email@domain.com"/>
+            <Controller
+                name="email"
+                control={control}
+                rules={{ required: true }}
+                render={(field) => {
+                    return(
+                        <TextField.Root type="email" placeholder="email@domain.com" {...field} />
+                    )
+                }}
+            />
         </TextField.Root>
+
+        {errors.email && <Text color="red">Email is required</Text>}
 
         <label htmlFor="password">Password</label>
         <TextField.Root >
             <TextField.Slot >
                 <LockClosedIcon height="16" width="16"/>
             </TextField.Slot>
-            <TextField.Root type="password" placeholder="*******"/>
+            <Controller
+                name="password"
+                control={control}
+                rules={{ required: true }}
+                render={(field) => {
+                    return(
+                        <TextField.Root type="password" placeholder="*******" {...field} />
+                    )
+                }}
+            />
         </TextField.Root>
+        {errors.password && <Text color="red">Password is required</Text>}
         <Button>
             Sign In
         </Button>
     </Flex>
+    </form>
   );
 }
 //seguir arreglando el formulario
