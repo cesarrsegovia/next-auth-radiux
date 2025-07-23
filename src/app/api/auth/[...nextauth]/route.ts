a9bd2d47ba3b6d23bee6768d0cd1a4a754e249fd
cesarrsegovia <cesarr.segovia27@gmail.com>
2025-07-23 10:22:38 -0300

    configurado next-auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {
+                email: { label: "Email", type: "email", placeholder: "asd@algo.com"},
+                password: { label: "Password", type: "password"}
+            },
+            async authorize(){
+                return null
+            }
+        })
+    ]
+});
+
+export { handler as GET, handler as POST };
