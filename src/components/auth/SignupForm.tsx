f1a31683d3d6e14b966ec52e4a40fdfe82fa17ae
cesarrsegovia <cesarr.segovia27@gmail.com>
2025-09-02 22:54:14 -0300

    arreglos

--- src/components/auth/SignupForm.tsx
+++ src/components/auth/SignupForm.tsx
@@ -12,7 +12,7 @@ function SignupForm() {
             password: ''
         }
     })
-    // asdadsdasasdsdfas
+    // asdad
     const onSubmit = handleSubmit(data => {
         console.log(data);
     });
