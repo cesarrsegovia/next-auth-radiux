c37dc701f4a7eedfea3a0db3acc96349d9633d06
cesarrsegovia <cesarr.segovia27@gmail.com>
2025-09-15 20:02:38 -0300

    agregada page de tareas

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,18 @@
+"use client"
+import { Button, Container, Heading } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
+
 function DashboardPage(){
+
+    const router = useRouter();
+
     return(
-        <div>Dashboard</div>
+        <Container className="mt-10">
+            <div className="flex justify-between">
+                <Heading>Tareas</Heading>
+                <Button onClick={()=> router.push('/dashboard/tasks/new')}>Agragar tarea</Button>
+            </div>
+        </Container>
     )
 }
 
---
+++ src/app/dashboard/tasks/new/page.tsx
@@ -0,0 +1,51 @@
+"use client"
+import { Button, Container, TextArea, TextField, Flex, Card, Heading } from "@radix-ui/themes"
+import { useForm, Controller } from "react-hook-form"
+
+function TaskNewPage() {
+
+    const {control, handleSubmit} = useForm({
+        values:{
+            title: '',
+            description: ''
+        }
+    });
+
+    const onSubmit = handleSubmit((data) => {
+        console.log(data)
+    })
+
+  return (
+    <div>
+        <Container size="1" height="100%" className="p-3 md:p-0">
+            <Flex className="h-screen w-full items-center">
+                <Card className="w-full p-7">
+                    <Heading>Crear proyecto</Heading>
+                    <form className="flex flex-col gap-y-2" onSubmit={onSubmit}>
+                        <label>Titulo del proyecto</label>
+                        <Controller 
+                            name="title"
+                            control={control}
+                            render={({field}) => {
+                                return <TextField.Root size="3" placeholder="Project title" {...field}/>
+                            }}
+                        />
+                        <label>Descripcion</label>
+                        <Controller 
+                            name="description"
+                            control={control}
+                            render={({field}) => {
+                                return <TextArea size="3" placeholder="Project description" {...field} />
+                            }}
+                        />
+
+                        <Button>Crear tarea</Button>
+                    </form>
+                </Card>
+            </Flex>
+        </Container>
+    </div>
+  )
+}
+
+export default TaskNewPage
